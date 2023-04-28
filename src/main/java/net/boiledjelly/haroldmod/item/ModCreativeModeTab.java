5591537aef4306a93d593c9a62c046ca07eb4be2
BoiledJelly <klzh.study@gmail.com>
2023-04-28 15:22:27 +0800

    Added Harold

--- src/main/java/net/boiledjelly/haroldmod/item/ModCreativeModeTab.java
+++ src/main/java/net/boiledjelly/haroldmod/item/ModCreativeModeTab.java
@@ -2,7 +2,6 @@ package net.boiledjelly.haroldmod.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import org.checkerframework.checker.units.qual.C;
 
 public class ModCreativeModeTab
 {
